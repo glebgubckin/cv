5129bd16c963288e20639479e9e793a5877c63f1
Gleb Gubkin <glebgubckin@yandex.ru>
2022-12-23 01:18:18 +0300

    add in progress cards style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,13 +4,15 @@ import styles from './card.module.scss'
 interface CardProps {
   title: string, 
   img: string,
+  progress?: boolean
 }
 
-const Card: FC<CardProps> = ({title, img}) => {
+const Card: FC<CardProps> = ({title, img, progress}) => {
   return (
     <div className={styles.wrapper}>
-      <img src={img} alt={title} />
-      <span className={styles.title}>{title}</span>
+      { progress && <span className={styles.progress}>Study Progress</span> }
+      <img className={progress ? styles.inprogress : ''} src={img} alt={title} />
+      <span className={progress ? `${styles.title} ${styles.inprogress}` : styles.title}>{title}</span>
     </div>
   )
 }
